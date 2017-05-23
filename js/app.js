dc0f4b51784a7830032f68c991d75e0f4cadd32c
Ana Durand <ana.dc1004@gmail.com>
2017-05-23 12:23:39 -0500

    cambio del doble arrow y checkout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+var ocultar = document.getElementsByClassName("ocultar");
+
+document.getElementById("checkout").addEventListener("click" , function(e){
+  e.preventDefault();
+  for(i=0; i<ocultar.length; i++){
+    ocultar[i].style.display="none";
+  }
+  document.getElementById("payment").style.display="block";
+});
+document.getElementById("cerrar").addEventListener("click", function(e){
+  e.preventDefault();
+  for(i=0; i<ocultar.length; i++){
+    ocultar[i].style.display="block";
+  }
+  document.getElementById("payment").style.display="none";
+});
